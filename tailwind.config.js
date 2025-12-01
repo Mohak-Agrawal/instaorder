0939c9c1a78afa6709cadd924e93e89c100087a3
Mohak Agrawal <54626046+Mohak-Agrawal@users.noreply.github.com>
2025-12-01 23:19:16 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6B35',
+        secondary: '#004E89',
+        accent: '#F77F00',
+      }
+    },
+  },
+  plugins: [],
+}
